e62641941e56728626574cf2d44dc00ad4434a31
seven <seven@uitox.com>
2017-06-28 11:17:17 +0800

    first commit

---
+++ testSignalR/App_Code/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        // 如需如何設定應用程式的詳細資訊，請參閱  http://go.microsoft.com/fwlink/?LinkID=316888
+        app.MapSignalR();
+    }
+}
